c4606c73bb4616670e23b019d3eb70d0578ec70f
brent.leemans <brent.leemans@bewire.be>
2021-04-24 20:06:22 +0200

    Add project

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './components/app/app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { PortfolioItemComponent } from './components/portfolio-item/portfolio-item.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PortfolioComponent,
+    PortfolioItemComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
